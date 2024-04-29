dc60e8d382ea4ef866bd7435b32331df088b2d0f
hackbyte07 <aayushranahardi@gmail.com>
2024-04-29 11:46:06 +0530

    working

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -1,8 +1,8 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from '../../Assets/sample.pdf'
+import pdf from "../../Assets/ayush rana resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
